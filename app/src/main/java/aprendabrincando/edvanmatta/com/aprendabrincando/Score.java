4c38d5eabc2b093dfaed31599065bb3bfa31114a
EdvanMatta <edvan.zaza10@gmail.com>
2017-05-04 16:49:10 -0300

    score

---
+++ app/src/main/java/aprendabrincando/edvanmatta/com/aprendabrincando/Score.java
@@ -0,0 +1,53 @@
+package aprendabrincando.edvanmatta.com.aprendabrincando;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Score extends AppCompatActivity {
+
+    private TextView nome;
+    private TextView pontosAcertos;
+    private TextView pontosErros;
+    private Button botaoVoltar;
+
+    private int pontosA;
+    private int pontosE;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score);
+
+        nome = (TextView) findViewById(R.id.txtNomeID);
+        pontosAcertos = (TextView) findViewById(R.id.txtPontoAID);
+        pontosErros = (TextView) findViewById(R.id.txtPontoEID);
+        botaoVoltar = (Button) findViewById(R.id.botaoVoltarID);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Intent it = getIntent();
+        String nomeExtra = it.getStringExtra("nome");
+        String pontosA = Integer.toString(it.getIntExtra("pontosAcertos",0));
+        String pontosE = Integer.toString(it.getIntExtra("pontosErros", 0));
+
+        nome.setText(nomeExtra);
+        pontosAcertos.setText(pontosA);
+        pontosErros.setText(pontosE);
+
+        botaoVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Score.this, MainActivity.class));
+                finish();
+            }
+        });
+    }
+}
